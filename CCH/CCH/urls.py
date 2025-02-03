6459bc19a728924118596e3b7574094bb1048ed5
Akira <yuzai.akira@gmail.com>
2025-02-03 19:54:59 +0330

    make login and logout view

--- CCH/CCH/urls.py
+++ CCH/CCH/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from challenge.views import HomeView
+from challenge.views import HomeView, LoginView, LogoutView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
--- CCH/challenge/views.py
+++ CCH/challenge/views.py
@@ -1,4 +1,27 @@
 from django.views.generic import TemplateView
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
-class HomeView(TemplateView):
+
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'challenge/home.html'
+
+
+class LoginView(LoginView):
+    template_name = 'challenge/login.html'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('home')
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password')
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+class LogoutView(LogoutView):
+    def get_success_url(self):
+        return reverse_lazy('home')
+
